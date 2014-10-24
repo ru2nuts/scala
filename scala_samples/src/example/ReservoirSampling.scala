5e733ae22c5192cf7dfd7beb3045753e9692c679
Alex Arutyunyants <alex@answr.com>
2014-10-24 00:10:43 -0400

    adding reservoir sampling

---
+++ scala_samples/src/example/ReservoirSampling.scala
@@ -0,0 +1,29 @@
+package example
+
+import scala.util.Random
+
+class ReservoirSampling {
+
+  def sample(size: Int, inputSizeLimit: Int, input: Iterator[Int]): Array[Int] = {
+    def r = new Random
+
+    def step(sampleArray: Array[Int], samplePos: Int, inputPos: Int, input: Iterator[Int]): Array[Int] = {
+      if (!input.hasNext)
+        sampleArray
+      else if (inputPos >= inputSizeLimit)
+        sampleArray
+      else if (samplePos < size) {
+        sampleArray(samplePos) = input.next()
+        step(sampleArray, samplePos + 1, inputPos + 1, input)
+      } else {
+        val nr = r.nextInt(inputPos)
+        if (nr < sampleArray.size) {
+          sampleArray(nr) = input.next()
+        }
+        step(sampleArray, samplePos + 1, inputPos + 1, input)
+      }
+    }
+    step(new Array[Int](size), 0, 0, input)
+  }
+
+}
---
+++ scala_samples/src/example/ScalaMain.scala
@@ -0,0 +1,22 @@
+package example
+
+import example.ReservoirSampling
+
+
+object ScalaMain {
+  def main(args: Array[String]) = {
+    def sm = new ScalaMain
+    sm.mnc(null)
+    (new ReservoirSampling).sample(3, 100, Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15).iterator).foreach(println(_))
+  }
+
+  def pr(s: String) =
+    println(s)
+
+}
+
+class ScalaMain {
+  def mnc(args: Array[String]) =
+    ScalaMain.pr("Hello world")
+
+}
