13aa6efb36d35117bce28e9523a8e58ed4a7c63d
Arutyunyants, Oleksandr (Alex) <Alex.Oleksandr@cengage.com>
2017-09-05 00:24:41 -0400

    more hr challenges

--- progfun/src/main/scala/org/ru2nuts/learn/hackerrank/ArrayManipulation.scala
+++ progfun/src/main/scala/org/ru2nuts/learn/hackerrank/ArrayManipulation.scala
@@ -1,34 +1,33 @@
 package org.ru2nuts.learn.hackerrank
 
-import scala.collection.immutable.IndexedSeq
-
 /**
   * https://www.hackerrank.com/challenges/crush
   */
 object ArrayManipulation {
 
-  def modifyA(a: Int, b: Int, k: Int, inA: IndexedSeq[Int]): IndexedSeq[Int] = {
-    for (i <- 0 to inA.length - 1) yield
-      if (i >= a && i <= b)
-        inA(i) + k
-      else
-        inA(i)
+  var max = 0L
+
+  def modifyA(a: Int, b: Int, k: Long, inA: scala.collection.mutable.ArraySeq[Long]) = {
+    for (i <- a to b) {
+      val l = inA(i) + k
+      if (l > max)
+        max = l
+      inA(i) = l
+    }
   }
 
   def main(args: Array[String]) {
     val sc = new java.util.Scanner(System.in);
-    val n = sc.nextInt();
-    val m = sc.nextInt();
-    var inA = (1 to n).map(i => 0)
+    val n = sc.nextInt
+    val m = sc.nextInt
+    var inA = new scala.collection.mutable.ArraySeq[Long](n)
 
     (1 to m).foreach(q => {
-      val a = sc.nextInt() - 1;
-      val b = sc.nextInt() - 1;
-      val k = sc.nextInt();
-      inA = modifyA(a, b, k, inA)
+      val a = sc.nextInt - 1
+      val b = sc.nextInt - 1
+      val k = sc.nextLong
+      modifyA(a, b, k, inA)
     })
-    var max = Integer.MIN_VALUE
-    inA.foreach(e => if (max < e) max = e)
     println(max)
   }
 
