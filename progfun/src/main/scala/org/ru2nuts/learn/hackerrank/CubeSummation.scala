b72fb1eceb8dac2d6a2d60d2d5bb88b71717fd27
Arutyunyants, Oleksandr (Alex) <Alex.Oleksandr@cengage.com>
2017-10-01 21:55:31 -0400

    cube sum sparse

--- progfun/src/main/scala/org/ru2nuts/learn/hackerrank/CubeSummation.scala
+++ progfun/src/main/scala/org/ru2nuts/learn/hackerrank/CubeSummation.scala
@@ -1,12 +1,40 @@
 package org.ru2nuts.learn.hackerrank
 
+import scala.collection.mutable
+
 /**
   * https://www.hackerrank.com/challenges/cube-summation
   */
 object CubeSummation {
 
-  def blockSum(cube: Array[Array[Array[Long]]], x1: Int, y1: Int, z1: Int, x2: Int, y2: Int, z2: Int): Long = {
-    cube.slice(x1 - 1, x2).map(l2 => l2.slice(y1 - 1, y2).map(l3 => l3.slice(z1 - 1, z2).sum).sum).sum
+  type cubeType = mutable.HashMap[Int, mutable.HashMap[Int, mutable.HashMap[Int, Long]]]
+
+  def setCubeVal(sparseCube: cubeType, x: Int, y: Int, z: Int, v: Long) = {
+    if (!sparseCube.contains(x))
+      sparseCube(x) = mutable.HashMap[Int, mutable.HashMap[Int, Long]]()
+    if (!sparseCube(x).contains(y))
+      sparseCube(x)(y) = mutable.HashMap[Int, Long]()
+    sparseCube(x)(y)(z) = v
+  }
+
+  def blockSum(sparseCube: cubeType, x1: Int, y1: Int, z1: Int, x2: Int, y2: Int, z2: Int): Long = {
+    var sum = 0L
+    sparseCube.keys.foreach(x => {
+      if (x1 <= x && x <= x2) {
+        val sx = sparseCube(x)
+        sx.keys.foreach(y => {
+          if (y1 <= y && y <= y2) {
+            val sxy = sx(y)
+            sxy.keys.foreach(z => {
+              if (z1 <= z && z <= z2) {
+                sum += sxy(z)
+              }
+            })
+          }
+        })
+      }
+    })
+    sum
   }
 
   def main(args: Array[String]) {
@@ -18,7 +46,7 @@ object CubeSummation {
       val n = sc.nextInt
       val m = sc.nextInt
 
-      val cube: Array[Array[Array[Long]]] = Array.ofDim[Long](n, n, n)
+      val sparseCube: cubeType = mutable.HashMap[Int, mutable.HashMap[Int, mutable.HashMap[Int, Long]]]()
 
       (1 to m).foreach(_ => {
         sc.next() match {
@@ -27,11 +55,11 @@ object CubeSummation {
             val y = sc.nextInt
             val z = sc.nextInt
             val W = sc.nextLong
-            cube(x - 1)(y - 1)(z - 1) = W
+            setCubeVal(sparseCube, x, y, z, W)
           }
           case "QUERY" => {
             val (x1, y1, z1, x2, y2, z2) = (sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt())
-            res += blockSum(cube, x1, y1, z1, x2, y2, z2)
+            res += blockSum(sparseCube, x1, y1, z1, x2, y2, z2)
           }
         }
       })
