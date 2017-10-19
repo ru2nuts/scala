9f21688a8689982cb6f728b89486d1bdde57326f
Arutyunyants, Oleksandr (Alex) <Alex.Oleksandr@cengage.com>
2017-10-19 15:14:47 -0400

    optimizing counting on tree

--- progfun/src/main/scala/org/ru2nuts/learn/hackerrank/CountingOnTree.scala
+++ progfun/src/main/scala/org/ru2nuts/learn/hackerrank/CountingOnTree.scala
@@ -1,75 +1,86 @@
 package org.ru2nuts.learn.hackerrank
 
-import scala.collection._
+import scala.collection.mutable
 
 /**
   * https://www.hackerrank.com/challenges/counting-on-a-tree
   */
 object CountingOnTree {
 
-  class Node(val index: Int, val value: Long, var unexplored: Boolean)
+  class Node(val index: Int, val value: Long) {
+    val adjacent: mutable.HashSet[Node] = mutable.HashSet[Node]()
 
-  class Edge(val from: Node, val to: Node)
-
-  val cache = scala.collection.mutable.Map[(Int, Int), Option[List[Node]]]()
-
-  def getPath(start: Node, end: Node, edges: Seq[Edge]): Option[List[Node]] = {
-    start.unexplored = false
-    if (start == end)
-      return Some(start :: Nil)
-
-    var pathIndex = (start.index, end.index)
-    val cachedVal = cache.get(pathIndex)
-    if (cachedVal.nonEmpty)
-      return cachedVal.get
-    else {
-      val frontier = edges.
-        filter(_.to.unexplored).
-        filter(_.from == start).
-        map(_.to).
-        union(edges.
-          filter(_.from.unexplored).
-          filter(_.to == start).
-          map(_.from))
-      if (frontier.nonEmpty) {
-        frontier.foreach(e => {
-          val p = getPath(e, end, edges)
-          if (p.nonEmpty) {
-            val result = Some(start :: p.get)
-            cache(pathIndex) = result
-            return result
-          }
-        })
-      }
-      cache(pathIndex) = None
-      return None
-    }
+    override def toString: String = "" + index + ":" + value + ": (" + adjacent.map(_.index).mkString(",") + ")"
   }
+
+  def getPath(currentNode: Node, prevNode: Node, end: Node): Option[List[Node]] = {
+    if (currentNode == end)
+      return Some(currentNode :: Nil)
+
+    val frontier = currentNode.adjacent.filter(_ != prevNode)
+    if (frontier.nonEmpty) {
+      frontier.foreach(e => {
+        val p = getPath(e, currentNode, end)
+        if (p.nonEmpty) {
+          val result = Some(currentNode :: p.get)
+          return result
+        }
+      })
+    }
+    return None
+  }
+
+  val valToIndexMap = mutable.HashMap[Long, mutable.HashSet[Int]]()
 
   def main(args: Array[String]) {
     val sc = new java.util.Scanner(System.in);
     val n = sc.nextInt
     val q = sc.nextInt
 
-    val nodes = (1 to n).map(index => (index, new Node(index, sc.nextLong, true))).toMap
+    val nodes = mutable.ArrayBuffer[Node]()
+    (1 to n).foreach(index => {
+      nodes += new Node(index, sc.nextLong)
+    })
 
-    val edges = (1 to n - 1).map(_ => (new Edge(nodes(sc.nextInt), nodes(sc.nextInt))))
+    (1 to n - 1).foreach(_ => {
+      val n1 = nodes(sc.nextInt - 1)
+      val n2 = nodes(sc.nextInt - 1)
+      n1.adjacent.add(n2)
+      n2.adjacent.add(n1)
+    })
 
-    val queries = (1 to q).map(_ => (nodes(sc.nextInt), nodes(sc.nextInt), nodes(sc.nextInt), nodes(sc.nextInt)))
+    val res = new StringBuilder
 
-    queries.foreach(q => {
-      val w = q._1
-      val x = q._2
-      val y = q._3
-      val z = q._4
+    (1 to q).map(_ => {
+      val w = nodes(sc.nextInt - 1)
+      val x = nodes(sc.nextInt - 1)
+      val y = nodes(sc.nextInt - 1)
+      val z = nodes(sc.nextInt - 1)
 
       //nodeValues
-      nodes.foreach(n => n._2.unexplored = true)
-      val pathWX: List[Node] = getPath(w, x, edges).getOrElse(Nil)
-      nodes.foreach(n => n._2.unexplored = true)
-      val pathYZ: List[Node] = getPath(y, z, edges).getOrElse(Nil)
+      val pathWX: List[Node] = getPath(w, w, x).getOrElse(Nil)
+      val pathYZ: List[Node] = getPath(y, y, z).getOrElse(Nil)
 
-      println(pathWX.filter(v1 => pathYZ.filter(v2 => v1.index != v2.index && v1.value == v2.value).size > 0).size)
+      valToIndexMap.clear()
+      var count = 0
+
+      pathWX.foreach(e => {
+        val s = valToIndexMap.getOrElseUpdate(e.value, new mutable.HashSet[Int]())
+        s.add(e.index)
+      })
+
+      pathYZ.foreach(e => {
+        val optVal = valToIndexMap.get(e.value)
+        if (optVal.nonEmpty) {
+          val v = optVal.get
+          if (v.size == 1 && v.head != e.index || v.size > 1)
+            count += 1
+        }
+      })
+
+      res.append(count).append('\n')
     })
+
+    println(res.toString())
   }
 }
