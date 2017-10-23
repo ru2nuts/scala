ad49de903ed52ee361ec11c0ee2877fc2a733698
Arutyunyants, Oleksandr (Alex) <Alex.Oleksandr@cengage.com>
2017-10-23 18:34:01 -0400

    coin problem solved

--- progfun/src/main/scala/org/ru2nuts/learn/hackerrank/CoinChange.scala
+++ progfun/src/main/scala/org/ru2nuts/learn/hackerrank/CoinChange.scala
@@ -7,30 +7,40 @@ import scala.collection.mutable
   */
 object CoinChange {
 
-  def combinationsCount(sum: Long, denominations: mutable.SortedSet[Long]): Int = {
-    if (denominations.size == 0 || sum == 0)
+  val cachedCounts = mutable.HashMap[(Long, Int), Long]()
+
+  def combinationsCount(sum: Long, denominations: Seq[Long], denomStartPos: Int): Long = {
+    if (denominations.size == denomStartPos || sum == 0)
       0
-    else if (denominations.size == 1) {
-      val den = denominations.head
+    else if (denominations.size == denomStartPos + 1) {
+      val den = denominations(denomStartPos)
       if (sum % den == 0)
         1
       else
         0
     } else {
-      var resultCount = 0
-      val largestDemon = denominations.head
-      val largestDemonCount = sum / largestDemon
+      val cachedRes = cachedCounts.get((sum, denomStartPos))
 
-      (1L to largestDemonCount).foreach(ldm => {
-        val rem = sum - largestDemon * ldm
-        if (rem == 0)
-          resultCount += 1
-        else
-          resultCount += combinationsCount(rem, denominations.tail)
-      })
+      if (cachedRes.nonEmpty)
+        cachedRes.get
+      else {
+        var resultCount = 0L
+        val largestDemon = denominations(denomStartPos)
+        val largestDemonCount = sum / largestDemon
 
-      resultCount += combinationsCount(sum, denominations.tail)
-      resultCount
+        (1L to largestDemonCount).foreach(ldm => {
+          val rem = sum - largestDemon * ldm
+          if (rem == 0)
+            resultCount += 1
+          else
+            resultCount += combinationsCount(rem, denominations, denomStartPos + 1)
+        })
+
+        resultCount += combinationsCount(sum, denominations, denomStartPos + 1)
+
+        cachedCounts.put((sum, denomStartPos), resultCount)
+        resultCount
+      }
     }
   }
 
@@ -43,6 +53,6 @@ object CoinChange {
     for (c_i <- 0 to m - 1) {
       c.add(sc.nextLong())
     }
-    println(combinationsCount(n, c))
+    println(combinationsCount(n, c.toSeq, 0))
   }
 }
