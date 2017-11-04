d7cf9d674ec7584824244a340b12d3be9d2be1b2
Arutyunyants, Oleksandr (Alex) <Alex.Oleksandr@cengage.com>
2017-11-03 23:35:03 -0400

    unique colors with bitsets

--- progfun/src/main/scala/org/ru2nuts/learn/hackerrank/UniqueColors.scala
+++ progfun/src/main/scala/org/ru2nuts/learn/hackerrank/UniqueColors.scala
@@ -1,6 +1,6 @@
 package org.ru2nuts.learn.hackerrank
 
-import scala.collection.mutable
+import scala.collection.{BitSet, mutable}
 
 /**
   * https://www.hackerrank.com/challenges/unique-colors
@@ -15,35 +15,39 @@ object UniqueColors {
 
   val cache: mutable.HashMap[(Node, Node), Int] = mutable.HashMap[(Node, Node), Int]()
 
-  def getPath(from: Node, to: Node, prevNode: Node, colors: mutable.HashSet[Int]): Int = {
+  def getPath(from: Node, to: Node, prevNode: Node, colors: BitSet): Int = {
 
-    colors += from.color
+    val newColors = colors + from.color
     if (from == to) {
-      val size = colors.size
+      val size = newColors.size
       return size
     }
-    val frontier = from.adjacent.filter(_ != prevNode)
+    val frontier = from.adjacent
     if (frontier.size == 0)
       return 0
     else {
       val size = frontier.map(e => { //foreach with var, because frontier is a hashset
-        getPath(e, to, from, getColorsClone(colors))
+        if (e == prevNode)
+          0
+        else
+          getPath(e, to, from, newColors)
       }).reduce(_ + _)
       return size
     }
-  }
-
-  private def getColorsClone(colors: mutable.HashSet[Int]) = {
-    colors.clone()
   }
 
   def main(args: Array[String]) {
     val sc = new java.util.Scanner(System.in);
     val n = sc.nextInt
 
+    var maxColors = 0
+
     val nodes = Array.ofDim[Node](n)
     (1 to n).foreach(index => {
-      nodes(index - 1) = new Node(index, sc.nextInt)
+      val colorNum = sc.nextInt
+      if (colorNum > maxColors)
+        maxColors = colorNum
+      nodes(index - 1) = new Node(index, colorNum)
     })
 
     (1 until n).foreach(zu => {
@@ -53,26 +57,27 @@ object UniqueColors {
       n2.adjacent += n1
     })
 
+    val res = new java.lang.StringBuilder()
+
     (0 until n).foreach(i => {
       val node1 = nodes(i)
       val colCount: Int = (0 until n).map(j => {
         val node2 = nodes(j)
-
-        val possibleVal1 = cache.get(node1, node2)
-        if (possibleVal1.nonEmpty) {
-          possibleVal1.get
+        val possibleVal2 = cache.get(node2, node1)
+        if (possibleVal2.nonEmpty) {
+          possibleVal2.get
         } else {
-          val possibleVal2 = cache.get(node2, node1)
-          if (possibleVal2.nonEmpty) {
-            possibleVal2.get
-          } else {
-            val size = getPath(node1, node2, node1, mutable.HashSet[Int]())
-            cache.put((node1, node2), size)
-            size
-          }
+          val b = BitSet.newBuilder
+          b.sizeHint(maxColors)
+          val size = getPath(node1, node2, node1, b.result())
+          cache.put((node1, node2), size)
+          size
         }
+
       }).reduce(_ + _)
-      println(colCount)
+      //println(colCount)
+      res.append(colCount).append('\n')
     })
+    println(res.toString())
   }
 }
